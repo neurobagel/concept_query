6efd98c79216abe34209eaae53ba06191c71fbfc
jarmoza <jarmoza@gmail.com>
2021-11-11 14:39:58 -0500

    Working download csv feature

--- query_prototype/views.py
+++ query_prototype/views.py
@@ -5,58 +5,88 @@
 # Imports
 
 # Standard library
+import ast
+import csv
+import datetime
 import json
 import os
 
 # Third party
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.http import StreamingHttpResponse # For large files
 
 # Custom
 from query.core import agg_dataset_info, create_query, process_query
 from .forms import QueryFieldsForm
 from .models import QueryFieldsModel
 # from examine_query_results import examine_query_results
-# from query_choices import *
+from .query_choices import *
 
+# Class for streaming http response for large files
+class Echo(object):
+    """An object that implements just the write method of the file-like
+    interface.
+    """
+    def write(self, value):
+        """Write the value by returning it, instead of storing in a buffer."""
+        return value
 
 # Helper functions
 
-def get_mock_results():
+def filtered_query_results(p_filtered_data, p_query_results):
 
-    modified_filename = "{0}{1}query_prototype{1}modified_query_results.json".format(os.getcwd(), os.sep)
-    with open(modified_filename, "r") as input_file:
-        query_results = json.loads(input_file.read())
+    # 0. Will store lists based on each subject in the query results that matches the filter
+    csv_data = []
 
-    return query_results
+    # 1. Save the header as the first row
+    header = [
+        "dataset_id",
+        "dataset_title",
+        "subject_id",
+        "age",
+        "gender",
+        "diagnosis",
+        "modality"
+    ]
+    csv_data.append(header)
 
-def get_manual_results():
+    # 2. Quickly construct a choices dictionary
+    choices = {
 
-    # a. Prepare data for query
-    # (NOTE: This should be done in future in form data cleaning function)
-    age_lower = int(form.cleaned_data["age_lower"])
-    age_upper = int(form.cleaned_data["age_upper"])
-    gender = form.cleaned_data["gender"].lower()
-    modality = "http://purl.org/nidash/nidm#" + form.cleaned_data["modality"]
-    diagnosis = ""
+        "gender": {choice[0]: choice[1] for choice in gender_choices},
+        "siri": {choice[0]: choice[1] for choice in diagnosis_choices},
+        "image": {choice[0]: choice[1] for choice in modality_choices},
+        "is_control": {choice[0]: choice[1] for choice in is_control_choices}
+    }
 
-    # b. Create a SPARQL query string with the query fields
-    sparql_query = create_query(
-        (age_lower, age_upper),
-        gender,
-        modality,
-        diagnosis,
-        "" )
+    # 3. Filter the query results
+    for result in p_query_results:
 
-    print("SPARQL QUERY")
-    print(sparql_query)
-    
-    # b. Query the graph database for datasets and subjects that fit 
-    # the field criteria
-    query_results = process_query(sparql_query)
+        # A. Only subjects in the filter datasets are selected
+        if result["dataset_id"] in p_filtered_data["datasets"]:
 
-    return query_results
+            # I. Each row will feature human-readable versions
+            result_row = [
+                result["dataset_id"],
+                "\"" + result["title"] + "\"",
+                str(len(csv_data)),
+                result["age"],
+            ]
 
-def get_correct_results(p_form):
+            # II. Save extra field values if present in the subject data
+            for field in choices.keys():
+                if field in result:
+                    result_row.append(result[field])
+                else:
+                    result_row.append("")
+
+            # III. Save the row
+            csv_data.append(result_row)
+
+    return csv_data
+            
+def get_query_results(p_form):
 
     # a. Create a SPARQL query string with the query fields
     sparql_query = create_query(
@@ -76,8 +106,8 @@ def get_correct_results(p_form):
     return query_results
 
 def labels_from_choices(p_choices):
-    return [choice[1] for choice in p_choices if "All" != choice[1]]
 
+    return [choice[1] for choice in p_choices if "All" != choice[1]]
 
 def process_results_into_datasets(p_query_results):
 
@@ -152,36 +182,23 @@ def formgenerated_query(request):
         if form.is_valid():
 
             # I. Look in the database to see if this query has already been performed
-            query_results = QueryFieldsModel.find_query_in_db(form.cleaned_data)
+            query_model_instance = QueryFieldsModel.find_query_in_db(form.cleaned_data)
+            if query_model_instance:
+                query_results = query_model_instance.results
             
             # II. If not and a new query must be performed            
-            if not query_results:
+            if not query_model_instance:
 
-                # Mock results (NOTE: To be removed)
-                # query_results = get_mock_results()
+                # a. Get SPARQL query results based on form inputs
+                query_results = get_query_results(form)
 
-                # Manually prepared results (NOTE: To be removed)
-                # query_results = get_manual_results()
-
-                # Correct flow
-                query_results = get_correct_results(form)
-
-                # Dump shortened version of results for examination
-                # temp_filepath = os.getcwd() + os.sep + "correct_results.json"
-                # with open(temp_filepath, "w") as output_file:
-                #     json.dump(query_results, output_file)
-                # examine_query_results(temp_filepath)
-
-                # Solution for adding extra fields to the database:
-                # https://stackoverflow.com/questions/58642207/save-extra-fields-to-django-model-apart-from-the-fields-which-were-present-in-dj
-
-                # c. Save the form data to the database without commit
+                # b. Save the form data to the database without commit
                 query_model_instance = form.save(commit=False)
 
-                # d. Add in the query results to the model object
+                # c. Add in the query results to the model object
                 query_model_instance.results = query_results
 
-                # e. Save the model object with query results to the database
+                # d. Save the model object with query results to the database
                 query_model_instance.save()
 
             # III. Respond with the form fields and query results
@@ -209,8 +226,9 @@ def formgenerated_query(request):
                 "query_results_new.html",
                 { 
                     "form": form,
-                    "results": results_for_template,
+                    "id": query_model_instance.id,
                     "query": True,
+                    "results": results_for_template,
                     "stats": stats,
                     "styles": styles
                 }
@@ -223,7 +241,46 @@ def formgenerated_query(request):
 
     # B. Respond with the blank form fields
     return render(request, 'new_base.html', {"form": form, "results": [], "query": False})
- 
-#  def download_csv(request):
 
-#      results = 
+def download_csv(request):
+
+    # 1. Get the dataset IDs in list form
+    decoded_request = request.body.decode("utf-8")
+    filtered_data = ast.literal_eval(decoded_request)
+
+    # 2. Get the query results for the corresponding query
+    query_model_instance = QueryFieldsModel.objects.get(pk=filtered_data["query"])
+    query_results = query_model_instance.results
+
+    # 3. Build filtered dataset for the csv file
+    csv_data = filtered_query_results(filtered_data, query_results)
+
+    # 4. Construct a CSV response
+    # For how the csv django response works, see:
+    # https://docs.djangoproject.com/en/3.2/howto/outputting-csv/
+    response = HttpResponse(headers = {
+        "Content-Type": "text/csv",
+        "Content-Disposition": "attachment; filename=results_new.csv"
+    })
+    # 'attachment; filename="results_new.csv"'.format(datetime.datetime.now())
+    # response["mime_type"] = "application/x-download"
+
+    # A. Create a csv writer that writes to the http response
+    writer = csv.writer(response)
+
+    # B. Write the csv header and filtered query results
+    for index in range(len(csv_data)):
+        writer.writerow(csv_data[index])
+
+    # # Streaming implementation
+    # pseudo_buffer = Echo()
+    # writer = csv.writer(pseudo_buffer)
+    # response = StreamingHttpResponse(
+    #     (writer.writerow(row) for row in csv_data),
+    #     content_type="text/csv",
+    #     mime_type='application/force-download'
+    # )
+    # response["Content-Disposition"] = "attachment; filename=\"results_new.csv\""
+
+    # C. Return response with csv file data
+    return response
