5d0b48331b8031f75c3c9731de764851ee4b178c
surchs <sebastian.urchs@gmail.com>
2021-11-24 15:48:52 -0500

    change basedir to proj root - closes #71 - also moves the database to the project root

--- concept_query_demo/settings.py
+++ concept_query_demo/settings.py
@@ -15,7 +15,7 @@ import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # TODO:
 #  Quick-start development settings - unsuitable for production
