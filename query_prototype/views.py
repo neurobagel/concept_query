325f932611942e4c8885a04a7e64c1adefc679e9
surchs <sebastian.urchs@gmail.com>
2021-11-24 15:39:02 -0500

    cleaning up query app views

--- query_prototype/views.py
+++ query_prototype/views.py
@@ -2,39 +2,25 @@
 # Creation date: October 25, 2021
 # Purpose: Possible views for concept query demo
 
-# Imports
-
 # Standard library
 import ast
 import csv
-import datetime
-import json
-import os
 
 # Third party
 from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse
-from django.http import StreamingHttpResponse # For large files
+from django.http import HttpResponse
 
 # Custom
 from query.core import agg_dataset_info, create_query, process_query
 from .forms import QueryFieldsForm
 from .models import QueryFieldsModel
-# from examine_query_results import examine_query_results
 from .query_choices import *
 
-# Class for streaming http response for large files
-class Echo(object):
-    """An object that implements just the write method of the file-like
-    interface.
-    """
-    def write(self, value):
-        """Write the value by returning it, instead of storing in a buffer."""
-        return value
 
 # Helper functions
-
 def filtered_query_results(p_filtered_data, p_query_results):
+    # TODO: create docstring
+    # This function creates the data for the csv download from the interface
 
     # 0. Will store lists based on each subject in the query results that matches the filter
     csv_data = []
@@ -53,7 +39,7 @@ def filtered_query_results(p_filtered_data, p_query_results):
 
     # 2. Quickly construct a choices dictionary
     choices = {
-
+        # TODO: replace siri with diagnosis here.
         "gender": {choice[0]: choice[1] for choice in gender_choices},
         "siri": {choice[0]: choice[1] for choice in diagnosis_choices},
         "image": {choice[0]: choice[1] for choice in modality_choices},
@@ -74,7 +60,7 @@ def filtered_query_results(p_filtered_data, p_query_results):
                 # result["age"],
             ]
 
-            # NOTE: Temp fix for results with no age field
+            # TODO: Temp fix for results with no age field, replace
             result_row.append("" if "age" not in result else result["age"])
 
             # II. Save extra field values if present in the subject data
@@ -88,8 +74,11 @@ def filtered_query_results(p_filtered_data, p_query_results):
             csv_data.append(result_row)
 
     return csv_data
-            
+
+
 def get_query_results(p_form):
+    # TODO: add docstring
+    # this function creates the SPARQL query from the form inputs in order to send it to the graph
 
     # a. Create a SPARQL query string with the query fields
     sparql_query = create_query(
@@ -103,7 +92,7 @@ def get_query_results(p_form):
         updrs=(p_form.cleaned_data["updrs_lower"], p_form.cleaned_data["updrs_upper"]),
         mmse=(p_form.cleaned_data["mmse_lower"], p_form.cleaned_data["mmse_upper"]),
     )
-
+    # TODO: replace the print statements with an actual logger
     print("SPARQL QUERY")
     print(sparql_query)
 
@@ -113,72 +102,12 @@ def get_query_results(p_form):
 
     return query_results
 
-def labels_from_choices(p_choices):
-
-    return [choice[1] for choice in p_choices if "All" != choice[1]]
-
-def process_results_into_datasets(p_query_results):
-
-    # 1. Gather dataset info and add subjects to each dataset dictionary
-    dataset_keys = {}
-    datasets = []
-    for subject in p_query_results["results"]["bindings"]:
-        if subject["title"] not in dataset_keys:
-            dataset_keys[subject["title"]] = len(dataset_keys)
-            datasets.append({
-                "title": subject["title"],
-                "description": subject["description"],
-                "subjects": []
-            })
-        index = dataset_keys[subject["title"]]
-        datasets[index]["subjects"].append({
-            "id":        len(datasets[index]["subjects"]) + 1,
-            "age":       subject["age"]["value"],
-            "modality":  subject["image"]["value"],
-            "gender":    subject["gender"]["value"],
-            "diagnosis": subject["siri"]["value"]
-        })
-
-    return datasets
 
 # Views
-
-def prototype_ui(request):
-
-        # return render(request, "base.html")
-
-        view_variables = {}
-        # view_variables["fields"] = prepopulate_function()
-        view_variables["fields"] = {
-
-            "gender": ["M", "F", "All"],
-            "modality": ["FlowWeighted", "DiffusionWeighted", "T1Weighted", "T2Weighted", "All"],
-            "is_control": ["Yes", "No", "All"]
-        }
-
-        if "POST" == request.method:
-
-            # Do query to Sebastian's code with form fields from request
-            # view_variables["query"] = sebs_query_function(form_fields)
-
-            # Mock
-            view_variables["query"] = {
-                
-                "age_lower":20,
-                "age_upper":55,
-                "gender":"F",
-                "modality": "T1Weighted",
-                "is_control": "No"
-            }
-
-            return render(request, "query_results.html", view_variables)
-
-        else:
-            # GET request
-
-            return render(request, "query_blank.html", view_variables)
-
 def formgenerated_query(request):
+    # TODO: add docstring
+    # - generates the blank interface
+    # - generates the results view once a response is coming in from the graph
 
     # 1. POST request handling
     if "POST" == request.method:
@@ -210,10 +139,11 @@ def formgenerated_query(request):
                 query_model_instance.save()
 
             # III. Respond with the form fields and query results
-            # results_for_template = process_results_into_datasets(query_results)
             results_for_template = agg_dataset_info(query_results)
 
             # IV. Extra information for populating results on the page
+            # TODO: revisit whether this should live in a separate place
+            # see issue #74
             styles = {
 
             }
@@ -223,10 +153,6 @@ def formgenerated_query(request):
 
                 "datasets": len(results_for_template),
                 "subjects": sum([result["n_subjects"] for result in results_for_template])
-                # "gender": { 
-                #     "labels": labels_from_choices(gender_choices),
-                #     "data": ...
-                # }
             }
 
             return render(
@@ -250,6 +176,7 @@ def formgenerated_query(request):
     # B. Respond with the blank form fields
     return render(request, 'new_base.html', {"form": form, "results": [], "query": False})
 
+
 def download_csv(request):
 
     # 1. Get the dataset IDs in list form
@@ -270,8 +197,6 @@ def download_csv(request):
         "Content-Type": "text/csv",
         "Content-Disposition": "attachment; filename=results_new.csv"
     })
-    # 'attachment; filename="results_new.csv"'.format(datetime.datetime.now())
-    # response["mime_type"] = "application/x-download"
 
     # A. Create a csv writer that writes to the http response
     writer = csv.writer(response)
@@ -279,16 +204,6 @@ def download_csv(request):
     # B. Write the csv header and filtered query results
     for index in range(len(csv_data)):
         writer.writerow(csv_data[index])
-
-    # # Streaming implementation
-    # pseudo_buffer = Echo()
-    # writer = csv.writer(pseudo_buffer)
-    # response = StreamingHttpResponse(
-    #     (writer.writerow(row) for row in csv_data),
-    #     content_type="text/csv",
-    #     mime_type='application/force-download'
-    # )
-    # response["Content-Disposition"] = "attachment; filename=\"results_new.csv\""
 
     # C. Return response with csv file data
     return response
