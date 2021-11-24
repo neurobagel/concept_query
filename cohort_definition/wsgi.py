d0e034c3e1f656935e09a14b760205631ba025fa
surchs <sebastian.urchs@gmail.com>
2021-11-24 18:45:07 -0500

    rename django project

--- cohort_definition/wsgi.py
+++ cohort_definition/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for concept_query_demo project.
+WSGI config for cohort_definition project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'concept_query_demo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cohort_definition.settings')
 
 application = get_wsgi_application()
