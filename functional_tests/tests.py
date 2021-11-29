33067343100df8f3988da6e9530356a4e29cd8f6
surchs <sebastian.urchs@gmail.com>
2021-11-29 18:10:01 -0500

    test for presence of web elements

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 # NOTE: the `live_server` fixture is defined as part of pytest-django and doesn't need to be created anywhere.
@@ -14,16 +15,25 @@ def driver():
 
 
 def test_user_can_run_an_empty_cohort_query(driver, live_server):
+    # TODO: find a better way to assert the location of elements (left / right)
+
     # I am a new user and want to check out the cohort definition tool
     # I navigate to the cohort_definition URL
     driver.get(live_server.url + '/query_interface/form')
 
-    # On the page, I see that the title of the app is "Cohort definition tool"
+    # On the page, I see that the title of the app is "Cohort Definition Tool"
     assert driver.title == 'Cohort Definition Tool'
 
-    # and I can see an interface on the left side of the window for defining my cohort,
-    # and on the right I see an empty area with a notification saying
-    # "please define a cohort and run a query to see results"
+    # and I can see a query interface on the left side of the window for defining my cohort,
+    vp_width = driver.get_window_size().get('width')
+    query_interface = driver.find_element(By.ID, 'query-fields-column')
+    assert query_interface.rect.get('x') < 0.05 * vp_width
+
+    # and on the right I see an empty area for the results
+    results_view = driver.find_element(By.ID, 'query-results-column')
+    assert vp_width - (results_view.rect.get('x') + results_view.rect.get('width')) < 0.05 * vp_width
+    # the results area has a note saying "Click 'Query Metadata' for results"
+    assert "Click 'Query Metadata' for results" in results_view.text
 
     # The interface allows me to define age, gender, diagnosis, and modality. I leave all of them emtpy
 
