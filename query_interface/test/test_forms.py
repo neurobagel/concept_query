815cbca928b1540740a707bd4176bee86488e36a
surchs <sebastian.urchs@gmail.com>
2021-12-02 18:41:34 -0500

    added basic form tests

---
+++ query_interface/test/test_forms.py
@@ -0,0 +1,35 @@
+import pytest
+
+from query_interface.forms import QueryFieldsForm
+
+
+def test_empty_query_form_is_valid():
+    form = QueryFieldsForm(data={})
+    assert form.is_valid(), 'The empty query form did not validate'
+
+
+@pytest.mark.parametrize("form_field",
+                         ("age_lower",
+                          "age_upper",
+                          "gender",
+                          "diagnosis",
+                          "modality",
+                          "is_control"
+                          )
+                         )
+def test_form_exposes_expected_field(form_field):
+    form = QueryFieldsForm()
+    assert form_field in form.fields, f'"{form_field}" was not in the query form fields'
+
+
+def test_age_cannot_be_negative():
+    test_data = {'age_lower': -1, 'age_upper': -1}
+    form = QueryFieldsForm(data=test_data)
+    assert not form.is_valid(), 'a negative age value did not invalidate the age form field'
+
+
+def test_that_diagnostic_choices_are_sorted_alphabetically():
+    form = QueryFieldsForm()
+    diagnosis = form['diagnosis'].field
+    choices = [label for code, label in diagnosis._get_choices()]
+    assert choices == sorted(choices), f'The diagnosis choices are not sorted alphabetically: \n{choices}'
